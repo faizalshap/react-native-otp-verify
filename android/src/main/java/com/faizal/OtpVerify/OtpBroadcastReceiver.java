351ad16b0826f5609cd21bb98d0ebf5b9ce2ec53
Vishesh Verma <visheshverma748@gmail.com>
2020-12-05 20:17:17 +0530

    Null pointer check applied
    Log.d internally uses the print method which needs not null value

--- android/src/main/java/com/faizal/OtpVerify/OtpBroadcastReceiver.java
+++ android/src/main/java/com/faizal/OtpVerify/OtpBroadcastReceiver.java
@@ -57,7 +57,9 @@ public class OtpBroadcastReceiver extends BroadcastReceiver {
                     // Get SMS message contents
                     String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
                     receiveMessage(message);
-                    Log.d("SMS", message);
+                    if (message != null) {
+                        Log.d("SMS", message);
+                    }
                     break;
                 case CommonStatusCodes.TIMEOUT:
                     Log.d("SMS", "Timeout error");
